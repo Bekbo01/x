dc3635f5d218d31e27f71144d9400db56b20dd68
Bekbolat Kurmetbek <59786257+Bekbo01@users.noreply.github.com>
2022-04-11 10:54:51 +0600

    Update settings.py

--- xtrade/settings.py
+++ xtrade/settings.py
@@ -142,7 +142,7 @@ CART_SESSION_ID = 'cart'
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
