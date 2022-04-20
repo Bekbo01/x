57d4b027915f816fd43df6a4be6c93cf22655fd0
Bekbolat Kurmetbek <59786257+Bekbo01@users.noreply.github.com>
2022-04-20 16:38:51 +0600

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -27,6 +27,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.CharField(max_length=2000) # models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image2 = models.CharField(max_length=2000)
+    image2 = models.CharField(max_length=2000)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=0)
     available = models.BooleanField(default=True)
